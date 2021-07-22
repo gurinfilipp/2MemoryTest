2e22617d2c7c4cf282e485a1ca0242576a50de99
Philip Gurin <gurinfilipp@gmail.com>
2021-07-22 21:21:16 +0300

    ready for refactoring

--- 2MemoryTest/MainViewController.swift
+++ 2MemoryTest/MainViewController.swift
@@ -25,65 +25,60 @@ class MainViewController: UIViewController {
         view.addSubview(tableView)
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        title = "Контакты"
+        
+        tableViewSetup()
+        fetchContacts()
+    }
+    
+    private func tableViewSetup() {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.tableFooterView = UIView()
         tableView.register(ContactCell.self, forCellReuseIdentifier: "ContactCell")
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
         
-        title = "Контакты"
-        
         let refreshControl = UIRefreshControl()
-        
         refreshControl.addTarget(self, action: #selector(didPullRefresh), for: .valueChanged)
-        
         tableView.refreshControl = refreshControl
-        
-        
-        fetchContacts()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
         tableView.pin.all()
     }
     
     private func fetchContacts() {
         let store = CNContactStore()
-        
+    
         store.requestAccess(for: .contacts) { access, error in
             if error != nil {
                 let alertController = UIAlertController (title: "Нет доступа к контактам", message: "Разрешить доступ к контактам в настройках?", preferredStyle: .alert)
-                    let settingsAction = UIAlertAction(title: "Настройки", style: .default) { (_) -> Void in
-                        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
-                            return
-                        }
-                        if UIApplication.shared.canOpenURL(settingsUrl) {
-                                   UIApplication.shared.open(settingsUrl, completionHandler: nil)
-                               }
+                let settingsAction = UIAlertAction(title: "Настройки", style: .default) { (_) -> Void in
+                    guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                        return
                     }
-                    alertController.addAction(settingsAction)
-                    let cancelAction = UIAlertAction(title: "Отмена", style: .default, handler: nil)
-                    alertController.addAction(cancelAction)
-
+                    if UIApplication.shared.canOpenURL(settingsUrl) {
+                        UIApplication.shared.open(settingsUrl, completionHandler: nil)
+                    }
+                }
+                alertController.addAction(settingsAction)
+                let cancelAction = UIAlertAction(title: "Отмена", style: .default, handler: nil)
+                alertController.addAction(cancelAction)
                 self.present(alertController, animated: true, completion: nil)
-                
             }
+            
             if access {
                 let keys = [CNContactGivenNameKey, CNContactPhoneNumbersKey]
                 let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
                 
                 try? store.enumerateContacts(with: request) { contact, stopPoint in
                     self.contactsArray.append(contact)
-                    
                 }
             } else {
                 self.tableView.refreshControl?.beginRefreshing()
             }
-            
         }
-        
     }
     
     @objc private func didPullRefresh() {
@@ -104,8 +99,7 @@ extension MainViewController: UITableViewDataSource, UITableViewDelegate {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath) as? ContactCell else {
             return .init()
         }
-    
-        
+
         cell.configure(with: contactsArray[indexPath.row])
         
         return cell
@@ -116,7 +110,7 @@ extension MainViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        print("calling \(contactsArray[indexPath.row].givenName)")
     }
     
 }
