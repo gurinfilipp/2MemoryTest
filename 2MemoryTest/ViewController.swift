a292012835ec3f0e2db9da6fffce4e12c5b588a4
Philip Gurin <gurinfilipp@gmail.com>
2021-07-22 19:23:52 +0300

    Initial Commit

---
+++ 2MemoryTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2MemoryTest
+//
+//  Created by Филипп Гурин on 22.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
